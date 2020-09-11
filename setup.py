4182577102e6e20edb4d792d0675741a5c7e62d4
Sean Falconer <sfalc@google.com>
2020-09-11 13:35:22 -0400

    Added copyright header

---
+++ setup.py
@@ -0,0 +1,38 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="google-businessmessages",
+    version="1.0.0",
+    author="Google LLC",
+    author_email="googleapis-packages@google.com",
+    description="Google's Business Messages API client library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/google-business-communications/python-businessmessages",
+    license="Apache 2.0",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
